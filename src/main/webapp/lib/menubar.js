41c17944fa1ff50a6b7087091db5800b16e74c18
Ankit <gaur.ankit2007@gmail.com>
2016-03-27 15:26:39 -0600

    Admin page menu bar fixes

--- src/main/webapp/lib/menubar.js
+++ src/main/webapp/lib/menubar.js
@@ -7,21 +7,39 @@ MenuBar = {
 			
 			if(this.isCollapsed)
 			{
-				$('#menubar').show();
+				$('#menubar').show('slow');
+				$('#menubar td a').animate({width: '14em'}, 'slow',function(){
+					
+				
+				});
+				
+				
+				
+				this.isCollapsed = false;
+				$('#detail_tab').removeClass('glyphicon-circle-arrow-right').addClass('glyphicon-circle-arrow-left');
+				
+				
+				/*$('#menubar').show();
 				$('.collapsable').animate({width: '14em'}, 'slow',function(){
 					$('.downicons').show();
 				});
 				this.isCollapsed = false;
-				$('#detail_tab').removeClass('glyphicon-circle-arrow-right').addClass('glyphicon-circle-arrow-left');
+				$('#detail_tab').removeClass('glyphicon-circle-arrow-right').addClass('glyphicon-circle-arrow-left');*/
 				
 				//$('#detail_tab').attr('src', 'img/mv-left.jpg');
 			}
 			else{
-				$('.downicons').hide();
+								
+				$('#menubar td a').animate({width: '0em'}, 'slow',function(){
+					$('#menubar').hide();
+				});
+				this.isCollapsed = true;
+				$('#detail_tab').removeClass('glyphicon-circle-arrow-left').addClass('glyphicon-circle-arrow-right');
+				/*$('.downicons').hide();
 				$('.collapsable').animate({width: '1em'}, 'slow');
 				this.isCollapsed = true;
 				//$('#detail_tab').attr('src', 'img/mv-right.jpg');
-				$('#detail_tab').removeClass('glyphicon-circle-arrow-left').addClass('glyphicon-circle-arrow-right');
+				$('#detail_tab').removeClass('glyphicon-circle-arrow-left').addClass('glyphicon-circle-arrow-right');*/
 			}			
 		}
 				
